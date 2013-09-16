00ded3805798c6322009424d88dde2a624c44d2a
alexirre <alex.1903@live.com.mx>
2013-09-16 16:57:51 +0200

    tarea 2

---
+++ conversor/conversor.php
@@ -0,0 +1,10 @@
+<?php
+ $dolar=15;
+ $cantidad=150;
+ $cambio;
+ echo "La cantidad es ".$cantidad."<br/>";
+ $cambio=$dolar*$cantidad;
+ echo "La cantidad de dolares equivale a ".$cambio." PESOS<br/>";
+ $cambio=$cantidad/$dolar;
+ echo "La cantidad de pesos equivale a ".$cambio." DOLARES<br/>";
+?>
